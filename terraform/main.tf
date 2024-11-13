a0cc8b512cec0f28c44f45cdbb1e34ccb9b2deef
ramsair <ramsaiummadisetty@gmail.com>
2024-11-13 22:34:36 +0000

    commited files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,18 @@
-# provider "azurerm" {
-#   features {}
-# }
-
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name   = azurerm_resource_group.rg.name
-#     storage_account_name   = azurerm_storage_account.storage.name
-#     container_name         = var.storage_container_name
-#     key                    = "terraform.tfstate"
-#   }
-# }
-
-# resource "azurerm_storage_account" "storage" {
-#   name                     = "tfstatestg"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier            = "Standard"
-#   account_replication_type = "LRS"
-# }
-
-# resource "azurerm_storage_container" "container" {
-#   name                  = "tfstatefiles"
-#   storage_account_name  = azurerm_storage_account.storage.name
-# }
-
 provider "azurerm" {
   features {}
 }
 
 variable "resource_group_name" {
-  default = "tf-linux-rg"
+  default = "network-rg"
 }
 
 variable "location" {
   default = "west US"
 }
 
+
 variable "vm_name" {
-  default = "tf-linux-vm"
+  default = "network-vm"
 }
 
 variable "admin_username" {
@@ -56,12 +31,12 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# Virtual Network
+# Virtual Network for vm
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vm_name}-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/23"]
+  address_space       = ["192.168.0.0/23"]
 }
 
 # Subnet
@@ -69,7 +44,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.vm_name}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["192.168.0.0/24"]
 }
 
 # Public IP Address
@@ -96,7 +71,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = "Network-NSG"
+  name                = "VM-NSG"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -140,6 +115,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
 
   admin_username = var.admin_username
 
+#authenticating using ssh public key
   # Use the generated SSH public key
   admin_ssh_key {
     username   = var.admin_username
@@ -170,68 +146,3 @@ output "private_key" {
   value     = tls_private_key.ssh_key.private_key_pem
   sensitive = true
 }
-
-
-
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "Network-VNET"
-#   address_space       = ["10.0.0.0/23"]
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-# }
-
-# resource "azurerm_subnet" "subnet" {
-#   name                 = "Infra-Subnet"
-#   resource_group_name  = azurerm_resource_group.rg.name
-#   virtual_network_name = azurerm_virtual_network.vnet.name
-#   address_prefixes     = ["10.0.0.1/24"]
-# }
-
-# resource "azurerm_public_ip" "pip" {
-#   name                = "Network-PIP"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   allocation_method   = "Static"
-#   sku                 = "Standard"
-# }
-
-# resource "azurerm_network_interface" "nic" {
-#   name                = "Network-NIC"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-
-#   ip_configuration {
-#     name                          = "internal"
-#     subnet_id                     = azurerm_subnet.subnet.id
-#     private_ip_address_allocation = "Dynamic"
-#     public_ip_address_id          = azurerm_public_ip.pip.id
-#   }
-# }
-
-# resource "azurerm_linux_virtual_machine" "vm" {
-#   name                = "Network-Infra-VM"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   size                = "Standard_B1s"
-#   admin_username      = var.admin_username
-#   admin_password      = var.admin_password
-#   network_interface_ids = [
-#     azurerm_network_interface.nic.id,
-#   ]
-
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
-
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "UbuntuServer"
-#     sku       = "18.04-LTS"
-#     version   = "latest"
-#   }
-
-#   disable_password_authentication = false
-# }
-
-
